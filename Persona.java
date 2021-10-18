5124f9cf09dbf9c2762819499366dc7050cea9a1
sandravelap <flippedsandra@gmail.com>
2021-10-18 13:26:43 +0200

    Add files via upload

---
+++ Persona.java
@@ -0,0 +1,25 @@
+package Files;
+
+import java.io.Serializable;
+
+public class Persona implements Serializable {
+	private String nombre;
+	private int edad;
+	
+	public Persona (String nombre, int edad) {
+		this.nombre=nombre;
+		this.edad = edad;
+	}
+	
+	public Persona () {
+		this.nombre = null;
+	}
+	
+	public void setNombre (String nombre) {this.nombre = nombre;}
+	public void setEdad (int edad) {this.edad = edad;}
+	
+	public String getNombre() {return this.nombre;}
+	public int getEdad() {return this.edad;}
+	
+	public String toString() {return ("Esta persona se llama "+this.nombre+" y su edad es "+Integer.toString(this.edad));}
+}
